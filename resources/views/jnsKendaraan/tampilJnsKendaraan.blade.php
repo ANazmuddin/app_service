74a0a06b730c44c88a0287ab8351ae166cdbb1a8
ANazmuddin <anazmuddin072@gmail.com>
2025-03-19 12:16:48 +0700

    upgrade CRUD

--- resources/views/jnsKendaraan/tampilJnsKendaraan.blade.php
+++ resources/views/jnsKendaraan/tampilJnsKendaraan.blade.php
@@ -36,39 +36,42 @@
           <div class="container-fluid">
             <!--begin::Row-->
             <div class="row">
-              <div class="col-md-6">
+              <div class="col-md-12">
                 <div class="card mb-4">
-                    <div class="card-header">
-                        <h3 class="card-title">Data Keseluruhan Jenis Kendaraan</h3>
-                    </div>
+                  <div class="card-header d-flex justify-content-between align-items-center">
+                      <h3 class="card-title m-right">Data Keseluruhan Jenis Kendaraan</h3>
+                      <a href="{{route('jnsKendaraan.create')}}" class="btn btn-primary">Tambah</a>
+                  </div>
                     <div class="card-body p-0">
-                                <a href="{{route('jnsKendaraan.create')}}">
-                            <input type="button" value="Tambah">
-                        </a>
-
-                        <table class="table table-striped">
-                            <tr>
-                                    <td>No</td>
-                                    <td>Nama Jenis Kendaraan</td>
-                                    <td>Aksi</td>
-                            </tr>
-                            @php $no = 1; @endphp
-                            @foreach($data as $row)
-
-                            <tr>
-                                <td>{{$no++}}</td>
-                                <td>{{$row->nm_jns_kendaraan}}</td>
-                                <td>
-                                    <a href="{{route('jnsKendaraan.edit',$row->id_jns_kendaraan)}}">update</a>  
-                                    <a href="{{route('jnsKendaraan.delete',$row->id_jns_kendaraan)}}">delete</a>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </table>
+                        <div class="table-responsive">
+                            <table class="table table-striped text-center">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama Jenis Kendaraan</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php $no = 1; @endphp
+                                    @foreach($data as $row)
+                                    <tr>
+                                        <td>{{$no++}}</td>
+                                        <td>{{$row->nm_jns_kendaraan}}</td>
+                                        <td>
+                                            <a href="{{route('jnsKendaraan.edit',$row->id_jns_kendaraan)}}" class="btn btn-warning btn-sm">Update</a>
+                                            <a href="{{route('jnsKendaraan.delete',$row->id_jns_kendaraan)}}" class="btn btn-danger btn-sm">Delete</a>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-                </div>
+                  </div>
               </div>
-            </div>
+          </div>
+
           </div>
         </div>
 
